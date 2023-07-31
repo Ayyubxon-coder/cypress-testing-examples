6b368343f4e74a53d3c427e8ab5816ba74cc0d6f
Ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2023-07-31 18:06:08 +0500

    secret menu is on working

--- cypress/integration/06-secret-menu.spec.js
+++ cypress/integration/06-secret-menu.spec.js
@@ -39,4 +39,33 @@ describe('Secret Menu Items', () => {
   it('should exist have the title on the page', () => {
     cy.get('h1').should('contain', 'Secret Menu Items');
   });
+
+  for (const property of properties) {
+    it(`should have a column ${property}`, () => {
+      cy.get(`#${property}-column`);
+    });
+
+    it(`should hide the ${property} column if unchecked`, () => {
+      cy.get(`#show-${property}`).click();
+      cy.get(`#${property}-column`).should('be.hidden');
+    })
+  }
+
+  for (const restaurant of restaurants) {
+    it(`should display only rowst that match of ${restaurant}`, () => {
+      cy.get('#restaurant-visibility-filter').select(restaurant);
+      cy.get('td[headers="whereToOrder-column"]').should('contain', restaurant).and('have.length.at.least', 1)
+    });
+  }
+  describe('Rating Filters', () => {
+      beforeEach(() => {
+        cy.get('#minimum-rating-visibility').as('rating')
+      });
+
+      for (const rating of ratings) {
+        it(`should only display items with rating of ${rating}`, () => {
+          cy.get('@rating').invoke('val', rating).trigger('change');
+        })
+      }
+  });
 });
